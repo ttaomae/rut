5bcc907038f74563bfc98e0b1a94b9027ce2d7d6
Todd Taomae <ttaomae@gmail.com>
2020-07-05 11:54:07 -1000

    Implement range parsing.

---
+++ src/range/mod.rs
@@ -0,0 +1,460 @@
+mod parse;
+use parse::ParseRangesError;
+use std::cmp::Ordering;
+use std::convert::From;
+use std::str::FromStr;
+use std::vec::Vec;
+
+/// A range of bytes, characters, or fields to select from the input.
+pub(crate) enum CutRange {
+    /// A single element.
+    Unit(usize),
+    /// A closed range of elements.
+    Closed(IncreasingRange),
+    /// All elements from the start to the specified end.
+    FromStart(usize),
+    /// All elements from the specified start to the end.
+    ToEnd(usize),
+}
+
+/// An increasing range.
+pub(crate) struct IncreasingRange {
+    pub(crate) start: usize,
+    pub(crate) end: usize,
+}
+
+impl IncreasingRange {
+    /// Creates a new `IncreasingRange` with the specified start and end.
+    fn new(start: usize, end: usize) -> IncreasingRange {
+        assert!(start <= end);
+        IncreasingRange { start, end }
+    }
+}
+
+/// A set of simplified and merged `CutRange`s. See [`MergedRange`].
+#[derive(Debug)]
+pub(crate) struct Ranges {
+    pub(crate) ranges: Vec<MergedRange>,
+}
+
+/// Simplified view of one or more merged `CutRange`s.
+#[derive(Copy, Clone, Debug)]
+pub(crate) enum MergedRange {
+    Closed(usize, usize),
+    ToEnd(usize),
+}
+
+impl From<&CutRange> for MergedRange {
+    fn from(range: &CutRange) -> Self {
+        match *range {
+            CutRange::FromStart(end) => MergedRange::Closed(0usize, end),
+            CutRange::Unit(n) => MergedRange::Closed(n, n),
+            CutRange::ToEnd(start) => MergedRange::ToEnd(start),
+            CutRange::Closed(IncreasingRange { start, end }) => MergedRange::Closed(start, end),
+        }
+    }
+}
+
+impl PartialEq for MergedRange {
+    fn eq(&self, other: &MergedRange) -> bool {
+        use MergedRange::{Closed, ToEnd};
+        match (self, other) {
+            (ToEnd(s1), ToEnd(s2)) => s1 == s2,
+            (Closed(s1, e1), Closed(s2, e2)) => s1 == s2 && e1 == e2,
+            _ => false,
+        }
+    }
+}
+
+impl Eq for MergedRange {}
+
+impl Ord for MergedRange {
+    fn cmp(&self, other: &Self) -> Ordering {
+        use MergedRange::{Closed, ToEnd};
+        match (self, other) {
+            (ToEnd(s1), ToEnd(s2)) => s1.cmp(s2),
+            (Closed(s1, e1), Closed(s2, e2)) => s1.cmp(s2).then(e1.cmp(e2)),
+            (ToEnd(s1), Closed(s2, _)) => s1.cmp(s2).then(Ordering::Greater),
+            (Closed(s1, _), ToEnd(s2)) => s1.cmp(s2).then(Ordering::Less),
+        }
+    }
+}
+
+impl PartialOrd for MergedRange {
+    fn partial_cmp(&self, other: &MergedRange) -> Option<Ordering> {
+        Option::Some(self.cmp(other))
+    }
+}
+
+/// A collection of ranges.
+impl Ranges {
+    /// Create a new `Ranges` from `CutRange`s.
+    pub(crate) fn from_ranges(ranges: &[CutRange]) -> Ranges {
+        if ranges.is_empty() {
+            return Ranges { ranges: Vec::new() };
+        }
+
+        // Simplify ranges and sort for easier merging.
+        let mut sorted_ranges: Vec<MergedRange> = ranges.iter().map(|cr| cr.into()).collect();
+        sorted_ranges.sort();
+
+        let mut result = Vec::new();
+
+        let mut ranges_iter = sorted_ranges.iter();
+        // Current range which may be merged with next range.
+        let mut merge_chain = *ranges_iter.next().unwrap_or(&MergedRange::ToEnd(0usize));
+
+        // Merge
+        for next in ranges_iter {
+            use MergedRange::{Closed, ToEnd};
+            match (merge_chain, next) {
+                // Ordering of MergedRanges ensures that all later ranges will start after the start
+                // of the current ToEnd, so all remaining ranges can be merged.
+                // e.g. 4-,4-8,6-,9-12 is reduced to 4-.
+                (ToEnd(_), _) => break,
+                (Closed(s1, e1), ToEnd(s2)) => {
+                    // assert s1 <= s2
+
+                    // Current Closed overlaps or touches next ToEnd, which we merge into ToEnd(s1).
+                    // Sort order ensure that all following values can also be merged (see comment
+                    // for previous match arm).
+                    if *s2 <= e1 + 1 {
+                        merge_chain = ToEnd(s1);
+                        break;
+                    }
+                    // No overlap. Add current range then stat a new merge chain.
+                    else {
+                        result.push(merge_chain);
+                        merge_chain = *next;
+                        break;
+                    }
+                }
+                (Closed(s1, e1), Closed(s2, e2)) => {
+                    // assert s1 <= s2
+
+                    // Ranges overlap or touch. Merge and continue.
+                    if *s2 <= e1 + 1 {
+                        merge_chain = Closed(s1, std::cmp::max(e1, *e2));
+                    }
+                    // No overlap. Add current range then start a new merge chain.
+                    else {
+                        result.push(merge_chain);
+                        merge_chain = *next;
+                    }
+                }
+            }
+        }
+
+        // Add final merge chain.
+        result.push(merge_chain);
+
+        Ranges { ranges: result }
+    }
+
+    pub(crate) fn elements(&self) -> impl Iterator<Item = &MergedRange> {
+        self.ranges.iter()
+    }
+}
+
+impl FromStr for Ranges {
+    type Err = ParseRangesError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        parse::parse(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_no_ranges() {
+        let ranges = Ranges::from_ranges(&[]);
+        assert_eq!(ranges.elements().next(), Option::None);
+    }
+
+    #[test]
+    fn test_from_single_range() {
+        use CutRange::{FromStart, ToEnd, Unit};
+
+        assert_simplify_to_single_range(&[Unit(0)], MergedRange::Closed(0, 0));
+        assert_simplify_to_single_range(&[Unit(3)], MergedRange::Closed(3, 3));
+        assert_simplify_to_single_range(&[Unit(9)], MergedRange::Closed(9, 9));
+        assert_simplify_to_single_range(&[Unit(15)], MergedRange::Closed(15, 15));
+
+        assert_simplify_to_single_range(&[closed(0, 0)], MergedRange::Closed(0, 0));
+        assert_simplify_to_single_range(&[closed(0, 1)], MergedRange::Closed(0, 1));
+        assert_simplify_to_single_range(&[closed(1, 4)], MergedRange::Closed(1, 4));
+        assert_simplify_to_single_range(&[closed(8, 32)], MergedRange::Closed(8, 32));
+
+        assert_simplify_to_single_range(&[FromStart(0)], MergedRange::Closed(0, 0));
+        assert_simplify_to_single_range(&[FromStart(1)], MergedRange::Closed(0, 1));
+        assert_simplify_to_single_range(&[FromStart(4)], MergedRange::Closed(0, 4));
+        assert_simplify_to_single_range(&[FromStart(8)], MergedRange::Closed(0, 8));
+
+        assert_simplify_to_single_range(&[ToEnd(0)], MergedRange::ToEnd(0));
+        assert_simplify_to_single_range(&[ToEnd(1)], MergedRange::ToEnd(1));
+        assert_simplify_to_single_range(&[ToEnd(4)], MergedRange::ToEnd(4));
+        assert_simplify_to_single_range(&[ToEnd(8)], MergedRange::ToEnd(8));
+    }
+
+    #[test]
+    fn test_simplify_sorted_to_single_range() {
+        use CutRange::{FromStart, ToEnd, Unit};
+
+        // Units only.
+        assert_simplify_to_single_range(&[Unit(0), Unit(1)], MergedRange::Closed(0, 1));
+        assert_simplify_to_single_range(&[Unit(5), Unit(6)], MergedRange::Closed(5, 6));
+        assert_simplify_to_single_range(
+            &[Unit(9), Unit(10), Unit(11), Unit(12)],
+            MergedRange::Closed(9, 12),
+        );
+
+        // Closed only.
+        assert_simplify_to_single_range(&[closed(0, 2), closed(3, 5)], MergedRange::Closed(0, 5));
+        assert_simplify_to_single_range(&[closed(0, 4), closed(4, 7)], MergedRange::Closed(0, 7));
+        assert_simplify_to_single_range(
+            &[closed(3, 6), closed(5, 8), closed(9, 10)],
+            MergedRange::Closed(3, 10),
+        );
+
+        // FromStart only.
+        assert_simplify_to_single_range(&[FromStart(2), FromStart(5)], MergedRange::Closed(0, 5));
+        assert_simplify_to_single_range(&[FromStart(6), FromStart(9)], MergedRange::Closed(0, 9));
+
+        // ToEnd only.
+        assert_simplify_to_single_range(&[ToEnd(4), ToEnd(6)], MergedRange::ToEnd(4));
+        assert_simplify_to_single_range(&[ToEnd(5), ToEnd(9)], MergedRange::ToEnd(5));
+
+        // Unit and Closed.
+        assert_simplify_to_single_range(&[Unit(0), closed(1, 4)], MergedRange::Closed(0, 4));
+        assert_simplify_to_single_range(&[closed(6, 9), Unit(10)], MergedRange::Closed(6, 10));
+        assert_simplify_to_single_range(
+            &[closed(12, 15), Unit(16), closed(17, 19)],
+            MergedRange::Closed(12, 19),
+        );
+
+        // FromStart and Unit.
+        assert_simplify_to_single_range(&[FromStart(3), Unit(3)], MergedRange::Closed(0, 3));
+        assert_simplify_to_single_range(&[FromStart(4), Unit(5)], MergedRange::Closed(0, 5));
+        assert_simplify_to_single_range(
+            &[FromStart(5), Unit(6), Unit(7)],
+            MergedRange::Closed(0, 7),
+        );
+
+        // FromStart and Closed.
+        assert_simplify_to_single_range(&[FromStart(3), closed(4, 5)], MergedRange::Closed(0, 5));
+        assert_simplify_to_single_range(&[FromStart(6), closed(3, 10)], MergedRange::Closed(0, 10));
+
+        // Unit and ToEnd.
+        assert_simplify_to_single_range(&[Unit(4), CutRange::ToEnd(4)], MergedRange::ToEnd(4));
+        assert_simplify_to_single_range(&[Unit(5), CutRange::ToEnd(6)], MergedRange::ToEnd(5));
+        assert_simplify_to_single_range(
+            &[CutRange::ToEnd(6), Unit(7), Unit(10)],
+            MergedRange::ToEnd(6),
+        );
+
+        // Closed and ToEnd.
+        assert_simplify_to_single_range(&[closed(3, 5), CutRange::ToEnd(5)], MergedRange::ToEnd(3));
+        assert_simplify_to_single_range(&[closed(5, 7), CutRange::ToEnd(6)], MergedRange::ToEnd(5));
+        assert_simplify_to_single_range(
+            &[CutRange::ToEnd(7), closed(8, 12)],
+            MergedRange::ToEnd(7),
+        );
+
+        // FromStart and ToEnd.
+        assert_simplify_to_single_range(&[FromStart(3), ToEnd(3)], MergedRange::ToEnd(0));
+        assert_simplify_to_single_range(&[FromStart(5), ToEnd(6)], MergedRange::ToEnd(0));
+        assert_simplify_to_single_range(&[FromStart(7), ToEnd(3)], MergedRange::ToEnd(0));
+
+        // All
+        assert_simplify_to_single_range(
+            &[Unit(0), FromStart(3), closed(4, 7), ToEnd(8)],
+            MergedRange::ToEnd(0),
+        );
+        assert_simplify_to_single_range(
+            &[FromStart(5), Unit(2), closed(4, 7), ToEnd(3)],
+            MergedRange::ToEnd(0),
+        );
+    }
+
+    #[test]
+    fn test_simplify_unsorted_to_single_range() {
+        use CutRange::{FromStart, ToEnd, Unit};
+
+        // Single type.
+        assert_simplify_to_single_range(
+            &[Unit(2), Unit(0), Unit(1), Unit(3)],
+            MergedRange::Closed(0, 3),
+        );
+        assert_simplify_to_single_range(
+            &[closed(3, 5), closed(1, 3), closed(6, 9)],
+            MergedRange::Closed(1, 9),
+        );
+        assert_simplify_to_single_range(
+            &[closed(7, 11), closed(5, 12), closed(9, 14)],
+            MergedRange::Closed(5, 14),
+        );
+        assert_simplify_to_single_range(&[FromStart(10), FromStart(5)], MergedRange::Closed(0, 10));
+        assert_simplify_to_single_range(&[ToEnd(6), ToEnd(4)], MergedRange::ToEnd(4));
+
+        // Mixed types.
+        assert_simplify_to_single_range(
+            &[Unit(6), closed(3, 8), Unit(9)],
+            MergedRange::Closed(3, 9),
+        );
+        assert_simplify_to_single_range(
+            &[Unit(5), FromStart(3), closed(4, 4)],
+            MergedRange::Closed(0, 5),
+        );
+        assert_simplify_to_single_range(&[ToEnd(10), closed(5, 8), Unit(9)], MergedRange::ToEnd(5));
+    }
+
+    #[test]
+    fn test_simplify_sorted_to_multiple_ranges() {
+        use CutRange::{FromStart, ToEnd, Unit};
+
+        // Single type.
+        assert_simplify_to_multiple_ranges(
+            &[Unit(1), Unit(2), Unit(6), Unit(7), Unit(15)],
+            &[
+                MergedRange::Closed(1, 2),
+                MergedRange::Closed(6, 7),
+                MergedRange::Closed(15, 15),
+            ],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[closed(3, 5), closed(6, 7), closed(10, 12), closed(11, 14)],
+            &[MergedRange::Closed(3, 7), MergedRange::Closed(10, 14)],
+        );
+
+        // Mixed types.
+        assert_simplify_to_multiple_ranges(
+            &[closed(2, 6), Unit(7), Unit(10), closed(10, 15)],
+            &[MergedRange::Closed(2, 7), MergedRange::Closed(10, 15)],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[FromStart(3), Unit(4), closed(7, 12), Unit(13)],
+            &[MergedRange::Closed(0, 4), MergedRange::Closed(7, 13)],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[FromStart(3), ToEnd(6)],
+            &[MergedRange::Closed(0, 3), MergedRange::ToEnd(6)],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[FromStart(2), Unit(4), ToEnd(8)],
+            &[
+                MergedRange::Closed(0, 2),
+                MergedRange::Closed(4, 4),
+                MergedRange::ToEnd(8),
+            ],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[FromStart(5), closed(10, 15), ToEnd(20)],
+            &[
+                MergedRange::Closed(0, 5),
+                MergedRange::Closed(10, 15),
+                MergedRange::ToEnd(20),
+            ],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[closed(2, 5), Unit(6), Unit(9), ToEnd(10)],
+            &[MergedRange::Closed(2, 6), MergedRange::ToEnd(9)],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[
+                FromStart(1),
+                closed(4, 6),
+                Unit(9),
+                closed(10, 12),
+                ToEnd(16),
+            ],
+            &[
+                MergedRange::Closed(0, 1),
+                MergedRange::Closed(4, 6),
+                MergedRange::Closed(9, 12),
+                MergedRange::ToEnd(16),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_simplify_unsorted_to_multiple_ranges() {
+        use CutRange::{FromStart, ToEnd, Unit};
+
+        // Single Type.
+        assert_simplify_to_multiple_ranges(
+            &[Unit(5), Unit(3), Unit(11), Unit(10), Unit(4)],
+            &[MergedRange::Closed(3, 5), MergedRange::Closed(10, 11)],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[closed(1, 4), closed(9, 11), closed(8, 10), closed(5, 6)],
+            &[MergedRange::Closed(1, 6), MergedRange::Closed(8, 11)],
+        );
+
+        // Multiple types.
+        assert_simplify_to_multiple_ranges(
+            &[closed(5, 6), closed(9, 12), Unit(3), closed(3, 4), Unit(10)],
+            &[MergedRange::Closed(3, 6), MergedRange::Closed(9, 12)],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[closed(9, 12), Unit(6), Unit(13), FromStart(5)],
+            &[MergedRange::Closed(0, 6), MergedRange::Closed(9, 13)],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[ToEnd(9), FromStart(4)],
+            &[MergedRange::Closed(0, 4), MergedRange::ToEnd(9)],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[closed(10, 15), FromStart(5), ToEnd(20), Unit(6)],
+            &[
+                MergedRange::Closed(0, 6),
+                MergedRange::Closed(10, 15),
+                MergedRange::ToEnd(20),
+            ],
+        );
+        assert_simplify_to_multiple_ranges(
+            &[
+                closed(12, 14),
+                Unit(15),
+                ToEnd(20),
+                Unit(4),
+                closed(19, 19),
+                FromStart(6),
+                closed(5, 8),
+                Unit(18),
+            ],
+            &[
+                MergedRange::Closed(0, 8),
+                MergedRange::Closed(12, 15),
+                MergedRange::ToEnd(18),
+            ],
+        );
+    }
+
+    fn assert_simplify_to_single_range(input_ranges: &[CutRange], expected_range: MergedRange) {
+        let actual_ranges = Ranges::from_ranges(input_ranges);
+        let mut elements = actual_ranges.elements();
+        assert_eq!(elements.next(), Option::Some(&expected_range));
+        assert_eq!(elements.next(), Option::None);
+    }
+
+    fn assert_simplify_to_multiple_ranges(
+        input_ranges: &[CutRange],
+        expected_ranges: &[MergedRange],
+    ) {
+        let actual_ranges = Ranges::from_ranges(input_ranges);
+        let mut elements = actual_ranges.elements();
+
+        for expected_range in expected_ranges {
+            assert_eq!(elements.next().unwrap(), expected_range);
+        }
+        assert_eq!(elements.next(), Option::None);
+    }
+
+    // Helper function to simplify the creation of CutRange::Closed.
+    fn closed(start: usize, end: usize) -> CutRange {
+        CutRange::Closed(IncreasingRange::new(start, end))
+    }
+}
---
+++ src/range/parse.rs
@@ -0,0 +1,284 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+use super::{CutRange, IncreasingRange, Ranges};
+
+#[derive(Debug)]
+pub(crate) enum Token {
+    Number(usize),
+    Hyphen,
+    Comma,
+    Blank(char),
+}
+
+#[derive(Debug)]
+pub(crate) enum ParseRangesError {
+    NumberedFromZero,
+    IndecipherableRange,
+    DescendingRange,
+    UnexpectedSeparator(Token),
+    LexError(LexError),
+}
+
+#[derive(Debug)]
+pub(crate) enum LexError {
+    UnrecognizedCharacter(char),
+}
+
+/// Parses a string into `Ranges`.
+pub(crate) fn parse(s: &str) -> Result<Ranges, ParseRangesError> {
+    match scan(s) {
+        Result::Ok(tokens) => parse_tokens(tokens),
+        Result::Err(e) => Result::Err(ParseRangesError::LexError(e)),
+    }
+}
+
+/// Scans a string into a `Vec` of [`Token`]s.
+fn scan(s: &str) -> Result<Vec<Token>, LexError> {
+    let mut tokens = Vec::new();
+    let mut chars = s.chars().peekable();
+    while let Some(ch) = chars.peek() {
+        match ch {
+            '-' => {
+                tokens.push(Token::Hyphen);
+                chars.next();
+            }
+            ',' => {
+                tokens.push(Token::Comma);
+                chars.next();
+            }
+            ' ' | '\t' => {
+                tokens.push(Token::Blank(*ch));
+                chars.next();
+            }
+            c if c.is_digit(10) => tokens.push(Token::Number(scan_number(&mut chars))),
+            _ => return Result::Err(LexError::UnrecognizedCharacter(*ch)),
+        }
+    }
+
+    Result::Ok(tokens)
+}
+
+/// Scans and consumes a number.
+fn scan_number(chars: &mut Peekable<Chars>) -> usize {
+    let mut number = String::new();
+
+    while let Some(ch) = chars.peek() {
+        if ch.is_digit(10) {
+            number.push(*ch);
+            chars.next();
+        } else {
+            break;
+        }
+    }
+
+    // Since we've only parsed digits, we know it is a valid usize.
+    number.parse::<usize>().unwrap()
+}
+
+/// Parses tokens into `Ranges`.
+fn parse_tokens(tokens: Vec<Token>) -> Result<Ranges, ParseRangesError> {
+    let mut cut_ranges = Vec::new();
+    let mut tokens = tokens.into_iter().peekable();
+    loop {
+        // Parse a single range.
+        let result = parse_range(&mut tokens);
+        match result {
+            Result::Ok(cut_range) => cut_ranges.push(cut_range),
+            Result::Err(e) => return Result::Err(e),
+        }
+        // Consume separator.
+        match tokens.peek() {
+            Option::Some(Token::Comma) | Option::Some(Token::Blank(_)) => {
+                tokens.next();
+            }
+            // No more tokens. Done parsing.
+            Option::None => break,
+            // Unexpected token.
+            _ => {
+                return Result::Err(ParseRangesError::UnexpectedSeparator(
+                    tokens.next().unwrap(),
+                ))
+            }
+        }
+    }
+
+    Result::Ok(Ranges::from_ranges(&cut_ranges))
+}
+
+/// Parse a single range from tokens.
+fn parse_range<I: Iterator<Item = Token>>(
+    tokens: &mut Peekable<I>,
+) -> Result<CutRange, ParseRangesError> {
+    // Collect all tokens until the next separator.
+    let mut range = Vec::new();
+    while let Some(token) = tokens.peek() {
+        match token {
+            Token::Hyphen | Token::Number(_) => range.push(tokens.next().unwrap()),
+            _ => break,
+        }
+    }
+
+    // Handle unit.
+    if range.len() == 1 {
+        return match range[0] {
+            Token::Number(n) => match n {
+                0 => Result::Err(ParseRangesError::NumberedFromZero),
+                _ => Result::Ok(CutRange::Unit(n - 1)),
+            },
+            _ => Result::Err(ParseRangesError::IndecipherableRange),
+        };
+    }
+    // Handle "-n" and "n-".
+    if range.len() == 2 {
+        return match (&range[0], &range[1]) {
+            (Token::Hyphen, Token::Number(end)) => match end {
+                0 => Result::Err(ParseRangesError::NumberedFromZero),
+                _ => Result::Ok(CutRange::FromStart(end - 1)),
+            },
+            (Token::Number(start), Token::Hyphen) => match start {
+                0 => Result::Err(ParseRangesError::NumberedFromZero),
+                _ => Result::Ok(CutRange::ToEnd(start - 1)),
+            },
+            _ => Result::Err(ParseRangesError::IndecipherableRange),
+        };
+    }
+    // Handle "n-m".
+    if range.len() == 3 {
+        return match (&range[0], &range[1], &range[2]) {
+            (Token::Number(start), Token::Hyphen, Token::Number(end)) => match (start, end) {
+                (0, _) | (_, 0) => Result::Err(ParseRangesError::NumberedFromZero),
+                _ if start <= end => {
+                    Result::Ok(CutRange::Closed(IncreasingRange::new(start - 1, end - 1)))
+                }
+                _ => Result::Err(ParseRangesError::DescendingRange),
+            },
+            _ => Result::Err(ParseRangesError::IndecipherableRange),
+        };
+    }
+
+    Result::Err(ParseRangesError::IndecipherableRange)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::range::{MergedRange, Ranges};
+
+    #[test]
+    fn test_parse_single_range() {
+        assert_parse_ranges("1", &[MergedRange::Closed(0, 0)]);
+        assert_parse_ranges("15", &[MergedRange::Closed(14, 14)]);
+
+        assert_parse_ranges("1-1", &[MergedRange::Closed(0, 0)]);
+        assert_parse_ranges("2-6", &[MergedRange::Closed(1, 5)]);
+        assert_parse_ranges("9-19", &[MergedRange::Closed(8, 18)]);
+
+        assert_parse_ranges("-1", &[MergedRange::Closed(0, 0)]);
+        assert_parse_ranges("-12", &[MergedRange::Closed(0, 11)]);
+
+        assert_parse_ranges("1-", &[MergedRange::ToEnd(0)]);
+        assert_parse_ranges("23-", &[MergedRange::ToEnd(22)]);
+    }
+
+    #[test]
+    fn test_parse_comma_separator() {
+        use MergedRange::{Closed, ToEnd};
+
+        assert_parse_ranges("1,2,3", &[Closed(0, 2)]);
+        assert_parse_ranges("5,10,20", &[Closed(4, 4), Closed(9, 9), Closed(19, 19)]);
+        assert_parse_ranges("4,8,3,2,9", &[Closed(1, 3), Closed(7, 8)]);
+
+        assert_parse_ranges("1-2,2-4", &[Closed(0, 3)]);
+        assert_parse_ranges("3-8,12-17", &[Closed(2, 7), Closed(11, 16)]);
+        assert_parse_ranges("14-21,5-10,4-6", &[Closed(3, 9), Closed(13, 20)]);
+
+        assert_parse_ranges("-3,-5", &[Closed(0, 4)]);
+        assert_parse_ranges("-8,-22,-6", &[Closed(0, 21)]);
+
+        assert_parse_ranges("5-,10-", &[ToEnd(4)]);
+        assert_parse_ranges("14-,3-,6-", &[ToEnd(2)]);
+    }
+
+    #[test]
+    fn test_parse_blank_separator() {
+        use MergedRange::{Closed, ToEnd};
+
+        assert_parse_ranges("1 2 3", &[Closed(0, 2)]);
+        assert_parse_ranges("5\t10\t20", &[Closed(4, 4), Closed(9, 9), Closed(19, 19)]);
+        assert_parse_ranges("4 8\t3\t2 9", &[Closed(1, 3), Closed(7, 8)]);
+
+        assert_parse_ranges("1-2 2-4", &[Closed(0, 3)]);
+        assert_parse_ranges("3-8\t12-17", &[Closed(2, 7), Closed(11, 16)]);
+        assert_parse_ranges("14-21 5-10\t4-6", &[Closed(3, 9), Closed(13, 20)]);
+
+        assert_parse_ranges("-3 -5", &[Closed(0, 4)]);
+        assert_parse_ranges("-8\t-22 -6", &[Closed(0, 21)]);
+
+        assert_parse_ranges("5-,10-", &[ToEnd(4)]);
+        assert_parse_ranges("14- 3-\t6-", &[ToEnd(2)]);
+    }
+
+    #[test]
+    fn test_parse_mixed_separator() {
+        use MergedRange::{Closed, ToEnd};
+
+        assert_parse_ranges("5,10-44 6-9\t-4", &[Closed(0, 43)]);
+        assert_parse_ranges(
+            "2 4\t6,8-10 -3",
+            &[Closed(0, 3), Closed(5, 5), Closed(7, 9)],
+        );
+        assert_parse_ranges("12-34 89-,11\t10", &[Closed(9, 33), ToEnd(88)]);
+        assert_parse_ranges("33-34\t9-14,-12 8", &[Closed(0, 13), Closed(32, 33)]);
+        assert_parse_ranges("23-54 123- 1,2,3\t4-30 40-130", &[ToEnd(0)]);
+    }
+
+    #[test]
+    fn test_parse_error() {
+        // Empty string.
+        assert!("".parse::<Ranges>().is_err());
+
+        // Unknown token.
+        assert!("x".parse::<Ranges>().is_err());
+        assert!("!".parse::<Ranges>().is_err());
+        assert!("1-#".parse::<Ranges>().is_err());
+        assert!("11-22 $-5".parse::<Ranges>().is_err());
+
+        // Indecipherable range.
+        assert!("1--".parse::<Ranges>().is_err());
+        assert!("23--".parse::<Ranges>().is_err());
+        assert!("--45".parse::<Ranges>().is_err());
+        assert!("67--89".parse::<Ranges>().is_err());
+        assert!("10-11-12".parse::<Ranges>().is_err());
+
+        // Multiple seprators.
+        assert!("13,,14".parse::<Ranges>().is_err());
+        assert!("15  16".parse::<Ranges>().is_err());
+        assert!("17\t\t18".parse::<Ranges>().is_err());
+        assert!("19, 20".parse::<Ranges>().is_err());
+        assert!("21 \t22".parse::<Ranges>().is_err());
+        assert!("23\t,24".parse::<Ranges>().is_err());
+
+        // Descending range.
+        assert!("9-8".parse::<Ranges>().is_err());
+        assert!("123-45".parse::<Ranges>().is_err());
+        assert!("1,5-3".parse::<Ranges>().is_err());
+        assert!("5-7\t43-21".parse::<Ranges>().is_err());
+        assert!("1,2 3\t4,98-76".parse::<Ranges>().is_err());
+
+        //Numbered from 0.
+        assert!("0,1,2".parse::<Ranges>().is_err());
+        assert!("0- 3-4".parse::<Ranges>().is_err());
+        assert!("-0\t4-".parse::<Ranges>().is_err());
+        assert!("0-1 5,6-7\t8-".parse::<Ranges>().is_err());
+    }
+
+    fn assert_parse_ranges(input: &str, expected: &[MergedRange]) {
+        let ranges: Ranges = input.parse().unwrap();
+        let mut elements = ranges.elements();
+
+        for expected_range in expected {
+            assert_eq!(elements.next().unwrap(), expected_range);
+        }
+        assert_eq!(elements.next(), Option::None);
+    }
+}
